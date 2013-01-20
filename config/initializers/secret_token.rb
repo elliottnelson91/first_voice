8f96bb630b4303e106b66577d9c2eddcbf34b994
Elliott Nelson <elliottnelson91@googlemail.com>
2013-01-20 00:23:38 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PowerApp::Application.config.secret_token = 'c3229b207241366ace82a465da27048f117f6ebb78a85dcda90060f336d43de8cd6f782ab45e4a0c3b42fcfef0ac982e3fe86495c7a4a9c23b313bf1306ed2e8'
